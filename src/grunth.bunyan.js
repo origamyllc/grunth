c03162488e35cd465a74be59f97c9f57b56649ce
Prashun Javeri <origamyllc@gmail.com>
2016-09-03 17:42:01 -0700

    Update grunth.bunyan.js

--- src/grunth.bunyan.js
+++ src/grunth.bunyan.js
@@ -1,19 +1,8 @@
 'use strict';
 
 var bunyan = require('bunyan');
-var logger = bunyan.createLogger({
-    name: 'fold-orchestrator',
-    streams: [
-        {
-            level: 'info',
-            stream: process.stdout            // log INFO and above to stdout
-        },
-        {
-            level: 'error',
-            stream: process.stdout   // log ERROR and above to a file
-        }
-    ]
-});
+var config = require('./../config');
+
+var logger = bunyan.createLogger(config.config);
 
 module.exports = logger;
-
