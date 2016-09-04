560b469c8b38652c9b6d6c2f2cc2c83dde0d80f3
Prashun Javeri <origamyllc@gmail.com>
2016-09-03 17:40:14 -0700

    cleaning code

--- src/grunth.bunyan.hook.js
+++ src/grunth.bunyan.hook.js
@@ -1,53 +1,8 @@
-var logger= require('./grunth.bunyan.js');
+'use strict';
 
-var hooker  = (function () {
-    // Instance stores a reference to the Singleton
-    var instance;
+var bunyan = require('bunyan');
+var config = require('./../config');
 
-    function init() {
-        return {
-            request: {},
-            hook: function (meta) {
-                this.request = meta;
-            },
-            info: function (message) {
-                delete this.request.message;
-                this.request.message = message;
-                logger.info(this.request)
-            },
-            error: function (message) {
-                delete this.request.message;
-                this.request.message = message;
-                logger.error(this.request)
-            },
-            debug: function (message) {
-                delete this.request.message;
-                this.request.message = message;
-                logger.debug(this.request)
-            },
-            warn: function (message) {
-                delete this.request.message;
-                this.request.message = message;
-                logger.warn(this.request)
-            }
-        }
+var logger = bunyan.createLogger(config.config);
 
-    };
-
-    return {
-        // Get the Singleton instance if one exists
-        // or create one if it doesn't
-        getInstance: function () {
-
-            if ( !instance ) {
-                instance = init();
-            }
-
-            return instance;
-        }
-
-    };
-
-})();
-
-module.exports = hooker.getInstance();
+module.exports = logger;
