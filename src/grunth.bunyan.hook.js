56c57badf534098d281a0fd348d411f60c9c6b08
Prashun Javeri <origamyllc@gmail.com>
2016-09-03 17:41:14 -0700

    updating files

--- src/grunth.bunyan.hook.js
+++ src/grunth.bunyan.hook.js
@@ -1,8 +1,34 @@
-'use strict';
+var log =  require('./grunth.bunyan.js');
 
-var bunyan = require('bunyan');
-var config = require('./../config');
+var $logger =(function (){
 
-var logger = bunyan.createLogger(config.config);
+    function grunth(options) {
+        log = options.log.child({req: options.req});
+    }
 
-module.exports = logger;
+    grunth.prototype.warn = function (message) {
+        log.warn(message)
+    }
+    grunth.prototype.fatal = function (message) {
+        log.fatal(message)
+    }
+    grunth.prototype.debug = function (message) {
+        log.debug(message)
+    }
+    grunth.prototype.info = function (message) {
+        log.info(message)
+    }
+    grunth.prototype.error = function (message) {
+        log.error(message)
+    }
+
+    return {
+        hook:function(req){
+          return  new grunth({log: log,req:req});
+        }
+    }
+})()
+
+
+
+module.exports = $logger;
