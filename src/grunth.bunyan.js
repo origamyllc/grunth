7d2fc5ad680e6430260b931aa272a8a0a50b9d9d
Prashun Javeri <origamyllc@gmail.com>
2016-09-04 07:20:39 -0700

    Update grunth.bunyan.js

--- src/grunth.bunyan.js
+++ src/grunth.bunyan.js
@@ -3,6 +3,17 @@
 var bunyan = require('bunyan');
 var config = require('./../config');
 
+function reqSerializer(req) {
+    return {
+        id:req.id,
+        method: req.method,
+        url: req.url,
+        headers: req.headers
+    };
+}
+
 var logger = bunyan.createLogger(config.config);
+
+logger.addSerializers({req: reqSerializer});
 
 module.exports = logger;
